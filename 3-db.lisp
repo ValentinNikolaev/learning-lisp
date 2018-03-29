95c0e920f27fadcbd18c073601579fc9f81244e3
ValentinNikolaev <valenti.nikolaev@gmail.com>
2018-03-29 22:12:39 +0300

    +3-db.f4 track

--- 3-db.lisp
+++ 3-db.lisp
@@ -3,9 +3,11 @@
 
 (defun simple-list ()
   (list 1 2 3))
-
 (defun simple-plist()
   (list :a 1 :b 2 :third-34 34))
 
 (defun get-simple-plist(el)
   (getf (simple-plist) el))
+
+(defun make-track (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
