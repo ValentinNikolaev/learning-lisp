5975b5ad823d47daa8a378540e144f7ce60cc923
ValentinNikolaev <valenti.nikolaev@gmail.com>
2018-03-30 10:22:24 +0300

    +3-db.f11 update

--- 3-db.lisp
+++ 3-db.lisp
@@ -66,4 +66,26 @@
   
   (remove-if-not selector-fn *db*))
 
-(defun foo_var_args (&key a (b 20) (c 30 c-p)) (list a b c c-p))
+(defun foo_var_args
+    (&key a (b 20) (c 30 c-p)) (list a b c c-p))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+    (and
+      (if title    (equal (getf cd :title)  title)  t)
+      (if artist   (equal (getf cd :artist) artist) t)
+      (if rating   (equal (getf cd :rating) rating) t)
+      (if ripped-p (equal (getf cd :ripped) ripped) t))))
+
+
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+	(mapcar
+	 #'(lambda(row)
+	     (when (funcall selector-fn row)
+	       (if title (setf (getf row :title) title))
+	       (if artist (setf (getf row :artist) artist))
+	       (if rating (setf (getf row :rating) rating))
+	       (if ripped-p (setf (getf row :ripped) ripped)))
+	     row)
+	 *db* )))
